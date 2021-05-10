d84226188ccfbb1df6ff1248f0e11ed43b789289
Pascal Mietlicki <p.mietlicki@gmail.com>
2021-05-10 16:53:57 +0200

    Bugfix folder path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
-if [ -f "/home/monero/config/config.json" ]
+if [ -f "/home/ravencoin/config/config.json" ]
 then
-./xmrig --config /home/monero/config/config.json
+./xmrig --config /home/ravencoin/config/config.json
 else
 ./xmrig --url=${POOL_URL} --user=${POOL_USER} --pass=${POOL_PW} -k --coin=${COIN}
 fi
