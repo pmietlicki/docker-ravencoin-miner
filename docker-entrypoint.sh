013cdc775532e8dc16dee863a66c19d8be137988
pmietlicki <p.mietlicki@gmail.com>
2021-05-11 19:14:50 +0200

    Simplification

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ex
-if [ -f "/home/ravencoin/config/config.json" ]
-then
-./xmrig --config /home/ravencoin/config/config.json
-else
-./xmrig --url=${POOL_URL} --user=${POOL_USER} --pass=${POOL_PW} -k --coin=${COIN}
-fi
-exec "$@"
